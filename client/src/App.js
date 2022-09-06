dbdf543f17ec59d59378b3c746f9d72c82825082
shanshan <sslee666999@gmail.com>
2022-09-06 03:06:40 -0400

    Submit!

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+
+const App = () => (
+  <GoogleOAuthProvider clientId="463622409928-lsbll748mvul1s8hetcodo9nduabd573.apps.googleusercontent.com">
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/auth" exact element={<Auth />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  </GoogleOAuthProvider>
+);
+
+export default App;
